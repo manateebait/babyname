d1bf123d5a81a645a9c29397d420a7c5c46682ac
manateebait <59706500+manateebait@users.noreply.github.com>
2025-06-15 10:24:50 -0400

    Update win-message.js

--- js/win-message.js
+++ js/win-message.js
@@ -1,8 +1,9 @@
 const WIN_MESSAGE = `
   <h2>🎉 You guessed it! 🎉</h2>
   <div style="margin-top: 20px; text-align: left; line-height: 1.6; font-size: 1.2em;">
-    <p><strong>Name:</strong> BATMAN Stratton</p>
-    <p><strong>Born:</strong> June 15th, 2025</p>
+    <p><strong>Name:</strong> BATMAN</p>
+    <p><strong>Born:</strong> September 19, 1992</p>
+    <p><strong>Location:</strong> Arkham</p>
     <p><a href='https://youtu.be/dQw4w9WgXcQ?si=Sv3iodR7Mbze_5l2' target='_blank' style='color: #57B9FF;'>📸 See the baby!</a></p>
   </div>`;
 
